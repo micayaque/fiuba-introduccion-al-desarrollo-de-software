736fc28fcf2a8d5a8b3df4d136ed6248ab250aa6
micayaque <myaque@dc.uba.ar>
2025-03-20 12:44:13 -0300

    add chequeo de archivo para hacer cat

--- tp1/consolidar.sh
+++ tp1/consolidar.sh
@@ -3,6 +3,8 @@
 while true; do
     for archivo in "$RUTA_A_EPN1/entrada"/*.txt; do #tomamos cada archivo en la carpeta entrada
 
+        [[ -e "$archivo" ]] || continue
+
         #awk '1; END { print "" }' "$archivo" >> "$RUTA_A_EPN1/salida/$FILENAME.txt"
         #printf "%s\n" "$(cat "$archivo")" >> "$RUTA_A_EPN1/salida/$FILENAME.txt"
 
