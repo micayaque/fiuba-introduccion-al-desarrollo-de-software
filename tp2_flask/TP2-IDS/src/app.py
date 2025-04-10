6de25ca0362e7eb06684ca51e90f621497779bc6
micayaque <myaque@dc.uba.ar>
2025-04-10 16:32:16 -0300

    renderizo los templates desde app.py

--- tp2_flask/TP2-IDS/src/app.py
+++ tp2_flask/TP2-IDS/src/app.py
@@ -1,3 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/menu.html')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
