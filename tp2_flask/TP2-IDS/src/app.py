a8a10d1a02d35e73cce4e7e7c920d5d609f237a6
micayaque <myaque@dc.uba.ar>
2025-04-10 20:38:56 -0300

    add diferentes titulos en header

--- tp2_flask/TP2-IDS/src/app.py
+++ tp2_flask/TP2-IDS/src/app.py
@@ -5,14 +5,22 @@ nombre_cafeteria = "Ghirardelli"
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('base.html', nombre_cafeteria=nombre_cafeteria)
+
+
 @app.route('/index')
 def index():
-    return render_template('index.html', nombre_cafeteria=nombre_cafeteria,)
+    titulo_header = "The Best Coffee Restaurant in Brooklyn"
+    return render_template('index.html', nombre_cafeteria=nombre_cafeteria, titulo_header=titulo_header)
 
 @app.route('/menu')
 def menu():
-    return render_template('menu.html', nombre_cafeteria=nombre_cafeteria)
+    titulo_header = "See Our Menu"
+    return render_template('menu.html', nombre_cafeteria=nombre_cafeteria, titulo_header=titulo_header)
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', nombre_cafeteria=nombre_cafeteria)
+    titulo_header = "Get in Touch"
+    return render_template('contact.html', nombre_cafeteria=nombre_cafeteria, titulo_header=titulo_header)
