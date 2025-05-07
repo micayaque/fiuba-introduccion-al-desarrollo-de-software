60746cab08547efc0148d0a06187fdad282edc69
micayaque <myaque@dc.uba.ar>
2025-05-07 08:38:35 -0300

    add algunos ejercicios de clases

---
+++ clases/primer_proyecto_flask/static/script.js
@@ -0,0 +1,6 @@
+function concatenar() {
+    const texto1 = document.getElementById("campo1").value;
+    const texto2 = document.getElementById("campo2").value;
+    const resultado = texto1 + texto2;
+    alert("El resultado es: " + resultado);    
+}
